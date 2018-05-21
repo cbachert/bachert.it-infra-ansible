4e7c2e54a5d69b2682302793e583b9b206bdd650
cbachert <cbachert@users.noreply.github.com>
2018-05-21 21:25:15 +0100

    Add eselgurke.local to backed up hosts

---
+++ roles/backup/files/etc/backuppc/eselgurke.local.pl
@@ -0,0 +1,7 @@
+$Conf{BackupFilesOnly} = {
+  '*' => [
+    '/mnt/eselgurke--hdd-data/truecrypt1/Projekte',
+    '/mnt/eselgurke--hdd-data/truecrypt1/ansible',
+    '/mnt/eselgurke--hdd-data/truecrypt1/Bilder'
+  ]
+};
