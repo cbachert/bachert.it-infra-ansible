c35783728102885ddf87875b7aa841ae866ef3f1
cbachert <cbachert@users.noreply.github.com>
2021-05-09 01:08:33 +0100

    Migrate backuppc to new server

--- roles/backuppc4/files/opt/mount-backup.sh
+++ roles/backuppc4/files/opt/mount-backup.sh
@@ -2,13 +2,13 @@
 echo "Setting hard drive idle time to 15 minutes"
 sudo hdparm -S 180 /dev/sda
 echo "Opening crypto device"
-sudo cryptsetup luksOpen /dev/disk/by-uuid/1ae70e66-8cf1-45a1-827a-d99b02b5fa02 BACKUPPC-PV0
+sudo cryptsetup luksOpen /dev/disk/by-uuid/907b24fe-4524-41d1-8b13-dd912f308146 BACKUPPC-PV0
 sleep 5
 echo "Activating volume groups"
 sudo vgchange -a y
 sleep 5
 echo "Mounting backup file system"
-sudo mount /dev/mapper/BACKUPPC--VG-BACKUPPC--LV--DATA /var/lib/backuppc
+sudo mount -o defaults,noatime,errors=remount-ro /dev/mapper/BACKUPPC--VG-BACKUPPC--LV--DATA /var/lib/backuppc
 #echo "Mounting swap file system"
 #sudo swapon /dev/BACKUPPC-VG/BACKUPPC-LV-SWAP
 echo "Start backuppc"
