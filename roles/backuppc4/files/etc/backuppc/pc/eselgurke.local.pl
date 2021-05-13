e2a3396e12387625dda5ad57344cc50546b51a4c
cbachert <cbachert@users.noreply.github.com>
2021-05-14 00:01:32 +0100

    BackupPC - Reduce wakeup schedule

--- roles/backuppc4/files/etc/backuppc/pc/eselgurke.local.pl
+++ roles/backuppc4/files/etc/backuppc/pc/eselgurke.local.pl
@@ -1,23 +1,3 @@
-#$Conf{RsyncShareName} = [
-#  '/mnt/eselgurke--hdd-data/Anwendungen',
-#  '/mnt/eselgurke--hdd-data/Auth',
-#  '/mnt/eselgurke--hdd-data/Backup',
-#  '/mnt/eselgurke--hdd-data/Bilder',
-#  '/mnt/eselgurke--hdd-data/Disks',
-#  '/mnt/eselgurke--hdd-data/Dokumente',
-#  '/mnt/eselgurke--hdd-data/Downloads',
-#  '/mnt/eselgurke--hdd-data/Einstellungen',
-#  '/mnt/eselgurke--hdd-data/Firma',
-#  '/mnt/eselgurke--hdd-data/Musik',
-#  '/mnt/eselgurke--hdd-data/Programmieren',
-#  '/mnt/eselgurke--hdd-data/Projekte',
-#  '/mnt/eselgurke--hdd-data/Scripts',
-#  '/mnt/eselgurke--hdd-data/Spiele',
-#  '/mnt/eselgurke--hdd-data/Text',
-#  '/mnt/eselgurke--hdd-data/Video',
-#  '/mnt/eselgurke--hdd-data/VM'
-#];
-
 $Conf{ClientNameAlias} = '192.168.0.99';
 
 $Conf{RsyncShareName} = [
